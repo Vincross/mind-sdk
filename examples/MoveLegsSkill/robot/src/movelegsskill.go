78226f9e00611235a15a248cb8051565de61db49
Yuejia Zhao <zyj@vincross.com>
2017-10-27 10:44:13 +0800

    Add some function with legposition.

--- examples/MoveLegsSkill/robot/src/movelegsskill.go
+++ examples/MoveLegsSkill/robot/src/movelegsskill.go
@@ -35,12 +35,29 @@ func ready() {
 	hexabody.Stand()
 	hexabody.MoveHead(0.0, FAST_DURATION)
 	// Using goroutines to make some commands be executed at the same time
-	go hexabody.MoveLeg(2, hexabody.NewLegPosition().SetCoordinates(-100, 50.0, 70.0), SLOW_DURATION)
+	legPosition2 := hexabody.NewLegPosition().SetCoordinates(-100, 50.0, 70.0)
+	// legPositionInfo() is used to check, adjust or return the legposition's infomation and it's not necessary.
+	legPositionInfo(legPosition2)
+	// Moveleg
+	go hexabody.MoveLeg(2, legPosition2, SLOW_DURATION)
 	hexabody.MoveLeg(5, hexabody.NewLegPosition().SetCoordinates(100, 50.0, 70.0), SLOW_DURATION)
 	go hexabody.MoveJoint(0, 1, 90, SLOW_DURATION)
 	hexabody.MoveJoint(0, 2, 45, SLOW_DURATION)
 	go hexabody.MoveJoint(1, 1, 90, FAST_DURATION)
 	hexabody.MoveJoint(1, 2, 45, FAST_DURATION)
+}
+
+func legPositionInfo(legPosition *hexabody.LegPosition) {
+	if !legPosition.IsValid() {
+		log.Info.Println("The position is not valid, means it's unreachale, fit it.")
+		legPosition.Fit()
+	}
+	x, y, z, err := legPosition.Coordinates()
+	if err != nil {
+		log.Info.Println("Get coordinates of legposition error:", err)
+	} else {
+		log.Info.Println("The coordinates of legposition are:", x, y, z)
+	}
 }
 
 func moveLegs(v float64) {
